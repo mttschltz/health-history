226a3259663d5c0e3bafc2290f9d90dcc02839c3
Matt Schultz <matt@quitenifty.com>
2024-07-18 02:12:34 +0000

    Display both parents on person page

--- sk/src/routes/person/[slug]/+layout.ts
+++ sk/src/routes/person/[slug]/+layout.ts
@@ -4,14 +4,27 @@ import type {
   PostsRecord,
   PostsResponse,
 } from "$lib/pocketbase/generated-types";
+import type exp from "constants";
 import type { LayoutLoad } from "./$types";
 
-export const load: LayoutLoad = async ({ params, fetch }) => {
+type PersonExpand = {
+  birthMother: PersonResponse;
+  birthFather: PersonResponse;
+};
+
+export const load: LayoutLoad = async ({ params, fetch, parent }) => {
   const { slug } = params;
   const filter = client.filter("id = {:slug}", { slug });
   const coll = client.collection("person");
-  const options = { fetch };
-  const record = await coll.getFirstListItem<PersonResponse>(filter, options);
+  const options = { fetch, expand: "birthMother,birthFather" };
+  const record = await coll.getFirstListItem<PersonResponse<PersonExpand>>(
+    filter,
+    options
+  );
+
+  const { metadata } = await parent();
+  metadata.title = metadata.headline = `${record.fullName}'s family`;
+
   return {
     record,
   };
--- sk/src/routes/person/[slug]/+page.svelte
+++ sk/src/routes/person/[slug]/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import ImgModal from "$lib/pocketbase/ImgModal.svelte";
   import { client } from "$lib/pocketbase/index.js";
+  import { base } from "$app/paths";
 
   const { data } = $props();
   const record = $derived(data.record);
+  const birthMother = $derived(record.expand?.birthMother);
+  const birthFather = $derived(record.expand?.birthFather);
   $effect(() => {
     data.metadata.title =
       data.metadata.headline = `${record.fullName}'s family`;
@@ -11,11 +14,35 @@
 </script>
 
 <article>
-  <!-- <pre class="body">{record.body}</pre> -->
-  <!-- {#each record.files ?? [] as file, index} -->
-  <!-- {@const src = client.files.getUrl(record, file)} -->
-  <!-- {@const title = `image ${index + 1} for: ${record.title}`} -->
-  <!-- <img {src} alt={title} {title} /> -->
-  <!-- <ImgModal {record} filename={file} /> -->
-  <!-- {/each} -->
+  <div class="flex flex-row items-center gap-3">
+    <div>Mother</div>
+    {#if birthMother}
+      <div>
+        <a href={`${base}/person/${birthMother.id}`} class="btn variant-filled"
+          >{birthMother.fullName}</a
+        >
+      </div>
+    {:else}
+      <p>Unknown</p>
+    {/if}
+  </div>
+  <div class="flex flex-row items-center gap-3">
+    <div>Father</div>
+    {#if birthFather}
+      <div>
+        <a href={`${base}/person/${birthFather.id}`} class="btn variant-filled"
+          >{birthFather.fullName}</a
+        >
+      </div>
+    {:else}
+      <p>Unknown</p>
+    {/if}
+    <!-- <pre class="body">{record.body}</pre> -->
+    <!-- {#each record.files ?? [] as file, index} -->
+    <!-- {@const src = client.files.getUrl(record, file)} -->
+    <!-- {@const title = `image ${index + 1} for: ${record.title}`} -->
+    <!-- <img {src} alt={title} {title} /> -->
+    <!-- <ImgModal {record} filename={file} /> -->
+    <!-- {/each} -->
+  </div>
 </article>
