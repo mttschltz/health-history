a4fe85c3990b120a668ff56a3a35aa1f29ece5a7
Matt Schultz <matt@quitenifty.com>
2024-07-19 03:22:59 +0000

    Remove lifestyle on person collection

---
+++ pb/pb_migrations/1721359357_updated_person.js
@@ -0,0 +1,65 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("1zp0krgfzaus89l")
+
+  // remove
+  collection.schema.removeField("5wsj0ntv")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "tkkqhlxj",
+    "name": "notes",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("1zp0krgfzaus89l")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5wsj0ntv",
+    "name": "lifestyleIssues",
+    "type": "select",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSelect": 3,
+      "values": [
+        "smoking",
+        "alcohol",
+        "job",
+        "overweight",
+        "underweight",
+        "other"
+      ]
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "tkkqhlxj",
+    "name": "lifestyleIssueDetails",
+    "type": "editor",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "convertUrls": false
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
